757c4ae10e8cebfd2b856abe0309a5ceb969a0ec
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-12 13:23:12 +0900

    5loop

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,29 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int now;
+                Console.Write("숫자를 입력해주세요:");
+                now = int.Parse(Console.ReadLine());
+
+
+                if (now > max)
+                {
+                    max = now;
+                }
+
+                if (i == 0)
+                {
+                    min = now;
+                }
+                else if (now < min) min = now;
+
+            }
+            Console.WriteLine("\n제일 큰 수는 " + max);
+            Console.WriteLine("제일 작은 수는" + min);
         }
     }
 }
